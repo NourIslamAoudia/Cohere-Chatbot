d4485b0c5bb4a2bab306f817b911b0a20b554d59
NorIslam04 <islamaoudi18@gmail.com>
2025-02-10 21:47:07 +0100

    change model to : command-r-08-2024

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.post('/chat', async (req, res) => {
 
     // Définir un timeout de 20 secondes pour éviter les blocages
     const response = await Promise.race([
-      cohere.chat({ message, model: 'command-r-plus-08-2024' }),
+      cohere.chat({ message, model: 'command-r-08-2024' }),
       new Promise((_, reject) => setTimeout(() => reject(new Error('Timeout: Cohere API took too long')), 20000)) // 20 sec timeout
     ]);
 
